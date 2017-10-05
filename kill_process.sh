fc1e8b874baddbe10b949014d8e6bb9eb0cb54a1
programmerzzx <programmerzzx@gmail.com>
2017-10-05 21:26:00 +0800

    kill one more process

--- kill_process.sh
+++ kill_process.sh
@@ -3,4 +3,5 @@
 read -p "输入进程名: " process
 
 #echo $process
-ps -ef | grep -i $process | awk ' NR == 1 { print $2; }' | xargs kill -9 | grep -i $process
+ps -ef | grep -i $process | awk '{ print $2; }' | xargs kill -9 
+#ps -ef | grep -i $process | awk ' NR == 1 { print $2; }' | xargs kill -9 | grep -i $process
