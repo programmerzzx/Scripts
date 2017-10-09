1bd8c32d5b3bfc7a614e363f48e5e9484c8a02f1
programmerzzx <programmerzzx@gmail.com>
2017-10-09 23:09:56 +0800

    kill_process.sh 增加输入判断

--- kill_process.sh
+++ kill_process.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# 输入进程名,终止进程
-read -p "输入进程名: " process
+# 进程名
+read -p "输入进程名: " pName
 
-#echo $process
-ps -ef | grep -i $process | awk '{ print $2; }' | xargs kill -9 
-#ps -ef | grep -i $process | awk ' NR == 1 { print $2; }' | xargs kill -9 | grep -i $process
+if [ -z $pName ];
+then 
+	echo "输入为空!!!"
+	exit
+fi
+
+ps -ef | grep -i $pName | awk '{ print $2; }' | xargs kill -9
+ps -ef | grep -i $pName
+
